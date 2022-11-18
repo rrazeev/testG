2b973c61603a3ce2db344706b1a1a23417138a93
rrazeev <lochan50@gmail.com>
2022-11-18 13:07:58 +0530

    Update multiply.py

--- multiply.py
+++ multiply.py
@@ -2,4 +2,4 @@ print("Logic for multiplication")
 print("Updated multiply new logic")
 print(" Adding new logic to master branch")
 
-print("Adding comment from github repo")
+print("Adding comment from github  remote repo")
